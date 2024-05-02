19a51988b24d031cf0b1eaf40919f52c63981e29
1ST26 <153452525+1ST26@users.noreply.github.com>
2024-05-02 10:36:42 +0530

    Exp2.py

--- DSAL Lab assignment 2_Group_A4_Code.cpp
+++ DSAL Lab assignment 2_Group_A4_Code.cpp
@@ -1,208 +1,82 @@
-/*🔘PROBLEM STATEMET:-
-Implement all the functions of a dictionary (ADT) using hashing and handle collisions using
-chaining with /without replacement.
-Data: Set of (key, value) pairs, Keys are mapped to values, Keys
-must be comparable, Keys must be unique
-Standard Operations: Insert(key, value),
-Find(key), Delete(key)*/
+class Hash:
+    def __init__(self):
+        self.d = [{'name': '', 'name1': ''} for _ in range(10)]
 
-/*
-#️⃣ QUICK REVISION NOTES:-
-🔹ADT stands for Abstract Data Type.
+    def insert(self):
+        print("\nEnter number of words:")
+        n = int(input())
+        for _ in range(n):
+            print("\nEnter the word:")
+            na = input()
+            print("\nEnter the meaning of that word:")
+            na1 = input()
+            sum_val = sum(ord(char) for char in na)
+            x = (sum_val // len(na)) % 10
+            print(x)
+            c = x
+            while True:
+                if self.d[x]['name'] == '':
+                    self.d[x]['name'] = na
+                    self.d[x]['name1'] = na1
+                    break
+                x = (x + 1) % 10
+                if c == x:
+                    print("\nHash table is full")
+                    break
 
-🔹It defines a set of operations that can be performed on the data structure, along with their behavior and properties.
-*/
+    def search(self):
+        print("\nEnter the word whose meaning you want:")
+        na = input()
+        sum_val = sum(ord(char) for char in na)
+        x = (sum_val // len(na)) % 10
+        c = x
+        while True:
+            if self.d[x]['name'] == na:
+                print("\nMEANING-> ", self.d[x]['name'], "=", self.d[x]['name1'])
+                break
+            x = (x + 1) % 10
+            if c == x:
+                print("\nWord not found")
+                break
 
-#include <iostream>
-#include <string.h>
-using namespace std;
+    def delete(self):
+        print("\nEnter the word which is to be deleted:")
+        na = input()
+        sum_val = sum(ord(char) for char in na)
+        x = (sum_val // len(na)) % 10
+        c = x
+        while True:
+            if self.d[x]['name'] == na:
+                print("\n", self.d[x]['name'], " word deleted")
+                self.d[x]['name'] = ''
+                self.d[x]['name1'] = ''
+                break
+            x = (x + 1) % 10
+            if c == x:
+                print("\nWord not found")
+                break
 
-class HashFunction
-{
-    typedef struct hash
-    {
-        long key;
-        char name[10];
-    } hash;
-    hash h[10];
+    def display(self):
+        for i in range(10):
+            print("\n", self.d[i]['name'], self.d[i]['name1'])
 
-public:
-    HashFunction();
-    void insert();
-    void display();
-    int find(long);
-    void Delete(long);
-};
 
-HashFunction::HashFunction()
-{
-    int i;
-    for (i = 0; i < 10; i++)
-    {
-        h[i].key = -1;
-        strcpy(h[i].name, "NULL");
-    }
-}
-void HashFunction::Delete(long k)
-{
-    int index = find(k);
-    if (index == -1)
-    {
-        cout << "\n\tKey Not Found";
-    }
-    else
-    {
-        h[index].key = -1;
-        strcpy(h[index].name, "NULL");
-        cout << "\n\tKey is Deleted";
-    }
-}
-int HashFunction::find(long k)
-{
-    int i;
-    for (i = 0; i < 10; i++)
-    {
-        if (h[i].key == k)
-        {
-            cout << "\n\t" << h[i].key << " is Found at " << i << " Location With Name " << h[i].name;
-            return i;
-        }
-    }
-    if (i == 10)
-    {
-        return -1;
-    }
-}
-void HashFunction::display()
-{
-    int i;
-    cout << "\n\t\tKey\t\tName";
-    for (i = 0; i < 10; i++)
-    {
-        cout << "\n\th[" << i << "]\t" << h[i].key << "\t\t" << h[i].name;
-    }
-}
-
-void HashFunction::insert()
-{
-    char ans, n[10], ntemp[10];
-    long k, temp;
-    int v, hi, cnt = 0, flag = 0, i;
-
-    do
-    {
-        if (cnt >= 10)
-        {
-            cout << "\n\tHash Table is FULL";
-            break;
-        }
-        cout << "\n\tEnter a Telephone No: ";
-        cin >> k;
-        cout << "\n\tEnter a Client Name: ";
-        cin >> n;
-        hi = k % 10; // hash function
-        if (h[hi].key == -1)
-        {
-            h[hi].key = k;
-            strcpy(h[hi].name, n);
-        }
-        else
-        {
-
-            if (h[hi].key % 10 != hi)
-            {
-                temp = h[hi].key;
-                strcpy(ntemp, h[hi].name);
-                h[hi].key = k;
-                strcpy(h[hi].name, n);
-                for (i = hi + 1; i < 10; i++)
-                {
-                    if (h[i].key == -1)
-                    {
-                        h[i].key = temp;
-                        strcpy(h[i].name, ntemp);
-                        flag = 1;
-                        break;
-                    }
-                }
-                for (i = 0; i < hi && flag == 0; i++)
-                {
-                    if (h[i].key == -1)
-                    {
-                        h[i].key = temp;
-                        strcpy(h[i].name, ntemp);
-                        break;
-                    }
-                }
-            }
-            else
-            {
-                for (i = hi + 1; i < 10; i++)
-                {
-                    if (h[i].key == -1)
-                    {
-                        h[i].key = k;
-                        strcpy(h[i].name, n);
-                        flag = 1;
-                        break;
-                    }
-                }
-                for (i = 0; i < hi && flag == 0; i++)
-                {
-                    if (h[i].key == -1)
-                    {
-                        h[i].key = k;
-                        strcpy(h[i].name, n);
-                        break;
-                    }
-                }
-            }
-        }
-        flag = 0;
-        cnt++;
-        cout << "\n\t..... Do You Want to Insert More Key: y/n";
-        cin >> ans;
-    } while (ans == 'y' || ans == 'Y');
-}
-
-int main()
-{
-    long k;
-    int ch, index;
-    char ans;
-    HashFunction obj;
-    do
-    {
-        cout << "\n\t***** Telephone (ADT) *****";
-        cout << "\n\t1. Insert\n\t2. Display\n\t3. Find\n\t4. Delete\n\t5. Exit";
-        cout << "\n\t..... Enter Your Choice: ";
-        cin >> ch;
-        switch (ch)
-        {
-        case 1:
-            obj.insert();
-            break;
-        case 2:
-            obj.display();
-            break;
-        case 3:
-            cout << "\n\tEnter a Key Which You Want to Search: ";
-            cin >> k;
-            index = obj.find(k);
-            if (index == -1)
-            {
-                cout << "\n\tKey Not Found";
-            }
-            break;
-        case 4:
-            cout << "\n\tEnter a Key Which You Want to Delete: ";
-            cin >> k;
-            obj.Delete(k);
-            break;
-        case 5:
-            break;
-        }
-        cout << "\n\t..... Do You Want to Continue in Main Menu:y/n ";
-        cin >> ans;
-    } while (ans == 'y' || ans == 'Y');
-}
+if __name__ == "__main__":
+    h = Hash()
+    while True:
+        print("\nEnter the choice:")
+        print("\n1. Insert word and its meaning")
+        print("2. Find meaning")
+        print("3. Delete the word")
+        print("4. Exit")
+        n = int(input())
+        if n == 1:
+            h.insert()
+        elif n == 2:
+            h.search()
+        elif n == 3:
+            h.delete()
+        elif n == 4:
+            break
+        else:
+            print("\nUnknown choice")
